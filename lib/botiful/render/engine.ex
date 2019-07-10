69caba113b4579ed41ad04ddd8dca96974e722fe
Daryl <daryl@blpc.us>
2019-07-09 21:04:36 -0400

    feat: agent to manage state demo

--- lib/botiful/render/engine.ex
+++ lib/botiful/render/engine.ex
@@ -10,6 +10,10 @@ defmodule Botiful.Render.Engine do
     GenServer.call(pid, :url)
   end
 
+  def swap_url(pid, new_url) do 
+    GenServer.cast(pid, {:swap_url, new_url})
+  end
+
   # Server Callbacks
   def init(url) do
     {:ok, %{url: url}}
@@ -17,5 +21,9 @@ defmodule Botiful.Render.Engine do
 
   def handle_call(:url, _from, state) do
     {:reply, state.url, state}
+  end
+
+  def handle_cast({:swap_url, new_url}, _state) do
+    {:noreply, %{url: new_url}}
   end
 end
